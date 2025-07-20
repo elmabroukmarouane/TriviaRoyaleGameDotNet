8f8fb6eb13821b365fef5aa7410ee82b67460c30
EL MABROUK Marouane <elmabroukmarouane@gmail.com>
2025-07-20 02:12:51 +0200

    Commit 2

---
+++ TriviaRoyaleGame.Api/Extensions/Add/AddAutoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace TriviaRoyaleGame.Api.Extensions.Add;
+
+public static class AddAutoMapper
+{
+    public static void AddAUTOMAPPER( this IServiceCollection self, Action<IMapperConfigurationExpression>? config = null, params Assembly[] assemblies)
+    {
+        if (assemblies == null || assemblies.Length == 0)
+        {
+            assemblies = [Assembly.GetExecutingAssembly()];
+        }
+
+        self.AddAutoMapper(cfg =>
+        {
+            config?.Invoke(cfg);
+        }, assemblies);
+    }
+}
